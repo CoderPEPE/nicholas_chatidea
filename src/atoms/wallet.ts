3697460bbd8b8073503e713e9007360b6dc796f0
codeicarus <esoph.dev.9@gmail.com>
2024-08-23 10:04:38 +0900

    feat: clear

--- src/atoms/wallet.ts
+++
@@ -1,7 +0,0 @@
-// state.js
-import { atom } from "recoil";
-
-export const walletAddressState = atom({
-  key: "walletAddressState",
-  default: "",
-});
--- src/wallet-adapter-wallets.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@solana/wallet-adapter-wallets' {
-    import { Wallet } from '@solana/wallet-adapter-base';
-  
-    export function getPhantomWallet(): Wallet;
-    export function getSolletWallet(): Wallet;
-  }
-  
